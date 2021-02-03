3ee25d4a8d430b34b261680c0519b776dd6ca93b
levannhan228 <lvnhan0897@gmail.com>
2021-02-03 10:13:22 +0700

    fix logo

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -13,7 +13,8 @@ function HomePage() {
       backgroundColor:'#ffff',
       borderRadius:'10px',
       width:'fit-content',
-      margin: '1rem auto'
+      margin: '1rem auto',
+      color:'black'
       }}>Homepage</Link>
       <p style={{fontSize:'14px'}}>The image is for illustrative purposes only</p>
       <img src="https://rawg.io/assets/images/cover.117cc320ec2800b9b12092ca23d6e86d.png" alt=""/>
